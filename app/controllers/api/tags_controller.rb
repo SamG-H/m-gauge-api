e423d7a0eae28ed17a115bc172a28149f4420ac6
Ro5hi <slen.detablan@gmail.com>
2021-04-26 12:02:07 -0600

    added models and tags controller

---
+++ app/controllers/api/tags_controller.rb
@@ -0,0 +1,18 @@
+class API::TagsController < ApplicationController 
+
+    def index
+    end
+     
+    def show
+    end
+
+    def new 
+    end 
+        
+    def create
+    end 
+
+    def destroy 
+    end 
+
+end 
---
+++ app/models/ProjectTag.rb
@@ -0,0 +1,3 @@
+class ProjectTag < ActiveRecord::Base
+
+end 
